3a9592be97051d8662b9050eb5b03c224963030f
Diego Wenndson Souza Costa <CIANDT\dwenndson@lnb035364bhz.ciandt.global>
2022-03-11 14:07:00 -0300

    Configuração swagger par visualização rotas

---
+++ src/main/java/com/ciandt/bootcamp/Java/Infra/SwaggerConfig.java
@@ -0,0 +1,43 @@
+package com.ciandt.bootcamp.Java.Infra;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.service.Documentation;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import static springfox.documentation.builders.PathSelectors.regex;
+
+import java.util.ArrayList;
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+    
+    @Bean
+    public Docket breweryApi(){
+        return new  Docket(DocumentationType.SWAGGER_2)
+        .select()
+        .apis(RequestHandlerSelectors.basePackage("com.java.BreweryApi"))
+        .paths(regex("/api.*"))
+        .build()
+        .apiInfo(metaInfo());
+    }
+    private ApiInfo metaInfo(){
+        return new ApiInfo(
+            "JAVA BreweryApi",
+            "API solictation brewery",
+            "1.0",
+            "Terms of Service",
+            new Contact("Ci&T", "url", "email"),
+            "Apache License Version 2.0",
+            "https://www.apache.org/licesen.html", new ArrayList<>()
+        );
+        
+    }
+}
