5012b9652cf5cf21f58abe35aeab361f742f6f4c
dwenndson <dwenndson@ciandt.com>
2022-03-14 14:42:08 -0300

    Configuração swagger para visualização de rotas

--- src/main/java/com/ciandt/bootcamp/Java/Infra/SwaggerConfig.java
+++ src/main/java/com/ciandt/bootcamp/Java/Infra/SwaggerConfig.java
@@ -2,29 +2,26 @@ package com.ciandt.bootcamp.Java.Infra;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.service.Contact;
-import springfox.documentation.service.Documentation;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
-
-import static springfox.documentation.builders.PathSelectors.regex;
 
 import java.util.ArrayList;
 
 @Configuration
 @EnableSwagger2
-public class SwaggerConfig {
+public class SwaggerConfig extends WebMvcConfigurationSupport{
     
     @Bean
     public Docket breweryApi(){
         return new  Docket(DocumentationType.SWAGGER_2)
         .select()
         .apis(RequestHandlerSelectors.basePackage("com.java.BreweryApi"))
-        .paths(regex("/api.*"))
         .build()
         .apiInfo(metaInfo());
     }
@@ -34,10 +31,16 @@ public class SwaggerConfig {
             "API solictation brewery",
             "1.0",
             "Terms of Service",
-            new Contact("Ci&T", "url", "email"),
+            new Contact("Ci&T", "ciandt.com", "ciandt@ciandt.com"),
             "Apache License Version 2.0",
             "https://www.apache.org/licesen.html", new ArrayList<>()
         );
-        
+    }
+    @Override
+    protected void addResourceHandlers(ResourceHandlerRegistry registry){
+        registry.addResourceHandler("swagger-ui.html")
+            .addResourceLocations("classpath:/META-INF/resources");
+        registry.addResourceHandler("/webjars/**")
+            .addResourceLocations("classpath:/META-INF/resources/webjars/");
     }
 }
